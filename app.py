7689f4db6d2818cfb1a068e01cca203cb438d4dd
Aidan McCaffrey <aidan@PRAdministrators-MacBook-Pro.local>
2019-09-30 16:53:49 -0400

    Changes to program

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from dash.dependencies import Input, Output, State
 
 heading = "Graph Viewer"
 title = "Enter parameters above to view the expression's graph below:"
-x_values = list(range(-10,11))
+x_values = [n*0.5 for n in range(-20, 21)]
 color = '#1a237e'
 name = 'f(x)'
 tabtitle = 'Graph Viewer'
@@ -50,7 +50,16 @@ app.layout = html.Div(children=[
     html.Div('y-intercept:'),
     dcc.Input(id='input-3', value=0, type='number'),
     html.Div('expression:'),
-    html.Div(id = 'expression'),
+    html.Div(id = 'expression', style={
+        'padding': '6px',
+        'font-size': '20px',
+        'height': '32px',
+        'width': '178px',
+        'font-style': 'italic',
+        'color': '#1a237e',
+        'backgroundColor': '#e5ecf6',
+        'textAlign': 'left',
+    }),
     dcc.Graph(
         id='figure-1',
         figure=fig
@@ -62,15 +71,15 @@ app.layout = html.Div(children=[
 ########## Define Callback
 @app.callback(
     [Output(component_id='figure-1', component_property='figure'),
-    Output(component_id='expression', component_property='children')],
+     Output(component_id='expression', component_property='children')],
     [Input(component_id='input-1', component_property='value'),
-    Input(component_id='input-2', component_property='value'),
-    Input(component_id='input-3', component_property='value')]
+     Input(component_id='input-2', component_property='value'),
+     Input(component_id='input-3', component_property='value')]
 )
 def update_graph(input_value1, input_value2, input_value3):
     trace = go.Scatter(
         x = x_values,
-        y = [(input_value1*x**input_value2 + input_value3) for x in x_values],
+        y = [(input_value1*(x**(input_value2)) + input_value3) for x in x_values],
         mode = 'lines',
         marker = {'color': color},
         name = name
@@ -84,9 +93,14 @@ def update_graph(input_value1, input_value2, input_value3):
         height=600
     )
 
-    # Generate the figure dictionary
+    # Generate the figure and expression dictionaries
     fig = go.Figure(data=data,layout=layout)
-    expression = f'{input_value1}x^{input_value2} + {input_value3}'
+
+    if input_value3 >= 0:
+        expression = f"{input_value1}x^{input_value2} + {input_value3}"
+    else:
+        expression = f"{input_value1}x^{input_value2} - {input_value3*(-1)}"
+
     return fig, expression
 
 ############ Deploy
